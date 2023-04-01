0648db9416200fcb8e8ba52942411e98e1eb2714
Hexx333 <116751160+Hexx333@users.noreply.github.com>
2023-04-01 15:35:54 +0200

    Add files via upload

---
+++ tgc.js
@@ -0,0 +1,24 @@
+console.log(
+(Math.random)(1) *101);
+ 110101
+   function play() {
+   let userNumber
+ get.ElementById(3)=value;                  
+ Message ;
+ }
+    let message;(33)
+     
+     if ( '9'> '6')  {
+     	[303 ^ 40047]
+         message = "yes od kors"
+     } else if (6< 8);
+     [36 ^ 9000]
+     	{
+        message = "1000000"  
+     } 
+      {
+        message = "Gratulacje, to właściwa liczba"
+     }
+
+document.getElementById("message").innerHTML = "101"
+
